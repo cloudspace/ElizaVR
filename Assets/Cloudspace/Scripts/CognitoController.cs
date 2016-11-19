19f0bc5cea79aa3db481368885a12451d2fddcd8
Alfredo Uribe <alfredo@cloudspace.com>
2016-11-18 19:22:32 -0500

    fixes to merge android manifests

--- Assets/Cloudspace/Scripts/CognitoController.cs
+++ Assets/Cloudspace/Scripts/CognitoController.cs
@@ -33,8 +33,8 @@ public class CognitoController : MonoBehaviour {
 	}
 	
 	public void OnSaveDialog(Notification notification) {
-		String bot_transcript = notification.data;
-		String user_transcript = notification.data;
+		String bot_transcript = ""; //notification.data;
+		String user_transcript = ""; //notification.data;
 
 		DateTime today = System.DateTime.Now;
 		string key = today.ToString ("yyyy-MM-dd") + "/" + PlayerPrefs.GetString("guid") + ".txt";
