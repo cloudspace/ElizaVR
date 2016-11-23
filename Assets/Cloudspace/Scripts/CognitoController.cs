0be3148b7af264d737e6843d0835adc44d0bbbb4
Rusty Phillips <rusty@cloudspace.com>
2016-11-23 17:31:20 -0500

    Fixed problem.

--- Assets/Cloudspace/Scripts/CognitoController.cs
+++ Assets/Cloudspace/Scripts/CognitoController.cs
@@ -36,7 +36,7 @@ public class CognitoController : MonoBehaviour {
 	public void OnSaveDialog(Notification notification) {
 		KeyValuePair<string, string> results = (KeyValuePair<string, string>)notification.data;
 		String bot_transcript = results.Value; //notification.data;
-		String user_transcript = results.Key//notification.data;
+		String user_transcript = results.Key; //notification.data;
 
 		DateTime today = System.DateTime.Now;
 		string key = today.ToString ("yyyy-MM-dd") + "/" + PlayerPrefs.GetString("guid") + ".txt";
